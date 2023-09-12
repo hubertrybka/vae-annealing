6a6d360ba93114504b37c6804e65d3f9d7d87538
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2023-09-12 11:39:40 +0200

    add baseline support

--- annealing.py
+++ annealing.py
@@ -5,17 +5,22 @@ class Annealer:
     """
     This class is used to anneal the KL divergence loss over the course of training VAEs.
     After each call, the step() function should be called to update the current epoch.
-    Parameters:
-        total_steps (int): Number of epochs to reach full KL divergence weight.
-        shape (str): Shape of the annealing function. Can be 'linear', 'cosine', or 'logistic'.
     """
 
-    def __init__(self, total_steps, shape, disable=False):
+    def __init__(self, total_steps, shape, baseline=0.0, disable=False):
+        """
+        Parameters:
+            total_steps (int): Number of epochs to reach full KL divergence weight.
+            shape (str): Shape of the annealing function. Can be 'linear', 'cosine', or 'logistic'.
+            baseline (float): Starting value for the annealing function [0-1]. Default is 0.0.
+        """
         self.total_steps = total_steps
         self.current_step = 0
         self.shape = shape
+        self.baseline = baseline
         if disable:
             self.shape = 'none'
+            self.baseline = 0.0
 
     def __call__(self, kld):
         """
@@ -29,20 +34,27 @@ class Annealer:
 
     def slope(self):
         if self.shape == 'linear':
-            slope_y = (self.current_step / self.total_steps)
+            y = (self.current_step / self.total_steps)
+            y = self.add_baseline(y)
         elif self.shape == 'cosine':
-            slope_y = (math.cos(math.pi * (self.current_step / self.total_steps - 1)) + 1) / 2
+            y = (math.cos(math.pi * (self.current_step / self.total_steps - 1)) + 1) / 2
+            y = self.add_baseline(y)
         elif self.shape == 'logistic':
             exponent = ((self.total_steps / 2) - self.current_step)
-            slope_y = 1 / (1 + math.exp(exponent))
+            y = 1 / (1 + math.exp(exponent))
+            y = self.add_baseline(y)
         elif self.shape == 'none':
-            slope_y = 1.0
+            y = 1.0
         else:
             raise ValueError('Invalid shape for annealing function. Must be linear, cosine, or logistic.')
-        return slope_y
+        return y
 
     def step(self):
         if self.current_step < self.total_steps:
             self.current_step += 1
         else:
             pass
+
+    def add_baseline(self, y):
+        y_out = y * (1 - self.baseline) + self.baseline
+        return y_out
